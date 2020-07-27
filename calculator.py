dd6c437a742673a7ab1d09721cbcda02107741b6
Rich Raffals <rich@awakenthemagic.com>
2020-07-27 01:26:59 -0700

    Add unit tests for calculator, and these files: .coveragerc (for pytest --cov), setup.cfg (for flake8), and requirements.txt

---
+++ calculator.py
@@ -0,0 +1,16 @@
+"""
+calculator.py
+Calculator library containing basic math operations.
+"""
+
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
